d14a14a583555f20c5cc745f63a9926690712603
DmitriyZanko <61140531+DmitriyZanko@users.noreply.github.com>
2020-05-17 22:10:38 +0300

    Add project files.

---
+++ ViewsSample/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsSample.Controllers
+{
+    public class Data
+    {
+        public int Counter { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["my_data"] = new Data
+            {
+                Counter = 10
+            };
+
+            ViewBag.Counter = 15;
+            ViewBag.UserName = "SeperAdmin";
+
+            TempData["my_data"] = new Data
+            {
+                Counter = 20
+            };
+
+            return View();
+        }
+
+        // Get request
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Data model)
+        {
+            return View();
+        }
+
+        public ActionResult Banner()
+        {
+            return View();
+        }
+    }
+}
